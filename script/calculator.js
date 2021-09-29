d3a6bbe0d658ac2a2a95a8067bf476634108efc1
ayyuce <ayyuce@londonist.co.uk>
2021-09-29 17:43:37 +0300

    almost done

--- script/calculator.js
+++ script/calculator.js
@@ -1,7 +1,6 @@
-
-var result=0;
-
 //dom variables
+var result = document.getElementById("result");
+var operations = document.getElementById("operations");
 var one = document.getElementById("one");
 var two = document.getElementById("two");
 var three = document.getElementById("three");
@@ -22,41 +21,101 @@ var divide = document.getElementById("divide");
 var dot = document.getElementById("dot");
 var equal = document.getElementById("equal");
 
-
-//this function clear result
-function clear(){
-  document.getElementId("result").value="";
-  document.getElementId("operations").value="";
-}
-//this function show values
-function show_values(){
-  document.getElementId("result").value+=value;
-}
-//this function add values
-function add(a,b){
-  return a+b;
-}
-//this function multiplication values
-function multiplication(a,b){
-  return a*b;
-}
-//this function divide values
-function divide(a,b) {
-  return a/b;
-}
-//this function find percent values
-function percent(a){
-  return a/100;
-}
-//this function find square root values
-function square_root(a){
-  return Math.sqrt(a);
-}
-
+// Container varialbes
+var equation = "";
 
 // Event lısteners
-sum.onclick = function() {
-  alert("calısıyor")
-  
+one.onclick = function() {
+  equation += "1";
+  operations.innerHTML = equation;
+}
 
+two.onclick = function() {
+  equation += "2";
+  operations.innerHTML = equation;
+}
+
+three.onclick = function() {
+  equation += "3";
+  operations.innerHTML = equation;
+}
+
+four.onclick = function() {
+  equation += "4";
+  operations.innerHTML = equation;
+}
+
+five.onclick = function() {
+  equation += "5";
+  operations.innerHTML = equation;
+}
+
+six.onclick = function() {
+  equation += "6";
+  operations.innerHTML = equation;
+}
+
+seven.onclick = function() {
+  equation += "7";
+  operations.innerHTML = equation;
+}
+
+eight.onclick = function() {
+  equation += "8";
+  operations.innerHTML = equation;
+}
+
+nine.onclick = function() {
+  equation += "9";
+  operations.innerHTML = equation;
+}
+
+zero.onclick = function() {
+  equation += "0";
+  operations.innerHTML = equation;
+}
+
+sum.onclick = function() {
+  equation += "+";
+  operations.innerHTML = equation;
+}
+
+square_root.onclick = function() {
+  equation += "√";
+  operations.innerHTML = equation;
+}
+
+percent.onclick = function() {
+  equation += "%";
+  operations.innerHTML = equation;
+}
+
+subtraction.onclick = function() {
+  equation += "-";
+  operations.innerHTML = equation;
+}
+
+multi.onclick = function() {
+  equation += "*";
+  operations.innerHTML = equation;
+}
+
+divide.onclick = function() {
+  equation += "/";
+  operations.innerHTML = equation;
+}
+
+dot.onclick = function() {
+  equation += ".";
+  operations.innerHTML = equation;
+}
+
+equal.onclick = function() {
+  dom_result = eval(equation);
+  result.innerHTML = dom_result;
+  equation = "";
+}
+clear.onclick = function() {
+  operations.innerHTML = "";
+  result.innerHTML = "0";
 }
